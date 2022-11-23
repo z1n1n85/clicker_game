efd5b3de46cc01955be938b658e56ff0681f6205
Zinin Ilya <metal-fox8@yandex.ru>
2022-11-24 01:31:13 +0300

    функционал разработан полностью

--- dist/main.js
+++ dist/main.js
@@ -3,20 +3,37 @@ const RECORD = document.getElementById('record');
 const TIMER = document.getElementById('timer');
 const SCORE = document.getElementById('score');
 
-const MAXTIME = 5000;
-const TICK = 10;
+const MAXTIME = 5000;  // таймер в милисекундах
+const TICK = 10;       // шаг у таймера
 
-let secTime = 0;
 let time = MAXTIME;     
 let scoreCount = 0;
-let record = 0;
+let recordCount = 0;
+let iMessage = 0; 
+let buttonMessage = ['Нужно тыкать быстро', 'У тебя только 5 сек.', 'Ты готов?'];
 
-BUTTON.onclick = () => {
-    gameInit();
+BUTTON.onclick = gameInstruction;
+
+function gameInstruction() {
+    iMessage < 3 ? BUTTON.firstChild.textContent = buttonMessage[iMessage]: gameSetup();
+    iMessage++;
+}
+
+function gameSetup() {
+    scoreCount = 0;
+    SCORE.textContent = 'Счёт: 00';
+    
+    time = MAXTIME;   
+    TIMER.textContent = 'Время: ' + (time / 1000).toFixed(2);
+    
+    BUTTON.firstChild.textContent = 'Старт';
+    BUTTON.onclick = gameInit;
 }
 
 function gameInit() {
     startTimer();
+    gameProcess();
+    BUTTON.firstChild.textContent = 'Тыкай!';
     BUTTON.onclick = gameProcess;
 }
 
@@ -35,9 +52,20 @@ function startTimer() {
 
 function gameProcess() {
     scoreCount++;
-    SCORE.textContent = 'Счёт: ' + scoreCount;
+    SCORE.textContent = scoreCount > 9 ? 'Счёт: ' + scoreCount: 'Счёт: 0' + scoreCount;
 }
 
 function gameEnd() {
+    BUTTON.firstChild.textContent = 'Конец!';
     BUTTON.setAttribute('disabled', true);
+    setTimeout(() =>{
+        BUTTON.removeAttribute("disabled");
+        BUTTON.firstChild.textContent = 'Заново?';
+    }, 3000);
+    BUTTON.onclick = gameSetup;
+
+    if (scoreCount > recordCount){
+        recordCount = scoreCount;
+        RECORD.textContent = recordCount > 9 ? 'Рекорд: ' + recordCount: 'Рекорд: 0' + recordCount;
+    }
 }
